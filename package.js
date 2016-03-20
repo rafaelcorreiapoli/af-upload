8350f26544cb56a439cf8773488fa3831cc578b8
shact.developer@gmail.com <shact.developer@gmail.com>
2016-03-20 20:53:10 -0300

    Added versions to packages

--- package.js
+++ package.js
@@ -7,16 +7,16 @@ Package.describe({
   git: 'https://github.com/rafaelcorreiapoli/af-upload',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
-  documentation: 'README.md'
+  documentation: null
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.2.1');
   api.use([
     'reactive-var',
-    'aldeed:autoform',
-    'fortawesome:fontawesome',
-    'cfs:ui',
+    'aldeed:autoform@5.8.1',
+    'fortawesome:fontawesome@4.5.0',
+    'cfs:ui@0.1.3',
     'es5-shim',
   ], 'client');
 
